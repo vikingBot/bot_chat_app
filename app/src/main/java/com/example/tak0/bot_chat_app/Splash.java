356e2a8f7295cb7ea19a618aa3a0551347b0aabf
tak0 <tak0auk@gmail.com>
2016-09-19 23:11:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tak0/bot_chat_app/Splash.java
@@ -0,0 +1,31 @@
+package com.example.tak0.bot_chat_app;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new splashHandler(), 1000);
+
+    }
+
+    class splashHandler implements Runnable {
+
+        @Override
+        public void run(){
+            Intent intent = new Intent(Splash.this, UserList.class);
+            startActivity(intent);
+            Splash.this.finish();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tak0/bot_chat_app/UserList.java
@@ -0,0 +1,17 @@
+package com.example.tak0.bot_chat_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tak0 on 2016/09/19.
+ */
+public class UserList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.user_list);
+
+    }
+}
